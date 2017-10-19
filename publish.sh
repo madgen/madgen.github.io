3a1258e26c51ad8425f1bf68306cca2487147418
Mistral Contrastin <me@madgen.net>
2017-10-19 19:07:02 +0100

    publish script

---
+++ publish.sh
@@ -0,0 +1,36 @@
+#! /usr/local/bin/bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack exec do-disturb-me clean
+stack exec do-disturb-me build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+rsync -a --filter='P _site/'      \
+         --filter='P _cache/'     \
+         --filter='P .git/'       \
+         --filter='P .gitignore'  \
+         --filter='P .stack-work' \
+         --delete-excluded        \
+         _site/ .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
